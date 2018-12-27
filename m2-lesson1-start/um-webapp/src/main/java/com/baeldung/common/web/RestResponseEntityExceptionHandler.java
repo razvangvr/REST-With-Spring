28e9398999634411343c5db2a5f8a5eb46b9c224
razvan <razvan.gaston@c4media.com>
2018-12-27 19:04:22 +0200

    Exception Handling, in case of a HTTP 400 Bad Request, return an error msg to the client so that he knows what the problem is

--- m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
+++ m2-lesson1-start/um-webapp/src/main/java/com/baeldung/common/web/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,16 @@
 package com.baeldung.common.web;
 
+import com.baeldung.common.web.exception.ApiError;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -17,4 +25,29 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
     // API
 
     // 400
+
+    @Override
+    protected final ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.info("Bad Request: " + ex.getMessage());
+        logger.debug("Bad Request: ", ex);
+
+        return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected final ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.info("Bad Request: " + ex.getMessage());
+        logger.debug("Bad Request: ", ex);
+
+        return handleExceptionInternal(ex, message(HttpStatus.BAD_REQUEST, ex), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+
+
+    private final ApiError message(final HttpStatus httpStatus, final Exception ex) {
+        final String message = ex.getMessage() == null ? ex.getClass().getSimpleName() : ex.getMessage();
+        final String devMessage = ExceptionUtils.getRootCauseMessage(ex);
+
+        return new ApiError(httpStatus.value(), message, devMessage);
+    }
 }
